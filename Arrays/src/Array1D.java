3a817107635e84657248434c7fd3a711a7de5330
ahmadfareedtech <ahmadfareed.work@gmail.com>
2023-01-03 12:04:01 +0500

    arrays module added

---
+++ Arrays/src/Array1D.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Array1D {
+    public static void main(String args[]) {
+        //Declaration of array
+        int[] arr; // reference variable created
+
+        //Instantiation of array
+        arr = new int[3]; // space allocated to all cells of array
+
+        //Initialization of array
+        arr[0] = 1; // value added to cell
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
